4d651cb540348e0daaa5787fa9972da7d4666c68
ridwandadu <90619148+ridwandadu@users.noreply.github.com>
2022-06-16 21:47:23 +0700

    delete directory

--- ridwandadu.github.io-master/brian4.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('#burger');
-const menu = document.querySelector('.nav-menu');
-
-burger.addEventListener('click' , function(){
-	burger.classList.toggle('aktip');
-	menu.classList.toggle('aktip')
-});
